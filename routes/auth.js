fb481f14bca79d20708b115c0ab2751b2f36efd9
NicoCartellone <65472865+NicoCartellone@users.noreply.github.com>
2021-07-25 11:49:47 -0300

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+//contraseña
+const bcrypt = require('bcrypt')
+
+//validacion
+const Joi = require('@hapi/joi')
+
+const schemaRegister = Joi.object({
+    name:Joi.string().min(6).max(255).required(),
+    email:Joi.string().min(6).max(255).required().email(),
+    password:Joi.string().min(6).max(255).required(),
+})
+
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/login', async(req, res) => {
+    //validaciones
+    const { error } = schemaLogin.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message })
+
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).json({error: true, mensaje:'Email no registrado'})
+
+    const passValida = await bcrypt.compare(req.body.password, user.password)
+    if(!passValida) return res.status(400).json({error: true, mensaje:'Error en contraseña'})
+
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, process.env.TOKEN_SECRET)
+
+    res.header('auth-token', token).json({
+        error: null,
+        data: {token}
+    })
+})
+
+router.post('/register', async(req, res) => {
+    
+    //validacion usuario
+    const {error} = schemaRegister.validate(req.body)
+    
+    if (error) {
+        return res.status(400).json({error: error.details[0].message})
+    }
+
+    const existeEmail = await User.findOne({email: req.body.email})
+    if(existeEmail) {
+        return res.status(400).json({error: true, mensaje: 'Email ya registrado'})
+    }
+
+    //hash contraseña
+    const saltos = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, saltos)
+
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: password
+    })
+
+    try {
+        const userDB = await user.save();
+        res.json({
+            error: null,
+            data: userDB
+        })
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+
+module.exports = router;
